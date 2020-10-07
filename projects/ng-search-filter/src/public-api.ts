b58a832ce9fa657f376dfd53deca379f7033a704
ismet kizgin <info@ismetkizgin.com>
2020-10-07 16:27:16 +0300

    fix: fix for filter pipe path

--- projects/ng-search-filter/src/public-api.ts
+++ projects/ng-search-filter/src/public-api.ts
@@ -1,7 +1,5 @@
 /*
  * Public API Surface of ng-search-filter
  */
-
-export * from './lib/ng-search-filter.service';
-export * from './lib/ng-search-filter.component';
+export * from './lib/ng-search-filter.pipe';
 export * from './lib/ng-search-filter.module';
